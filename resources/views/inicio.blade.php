040715519651d2469a20461aabd19ad86e0dd7a4
Drooyy <alexandersn1313@gmail.com>
2023-09-06 10:56:38 -0500

    eh agregado un h1, p1, @php and @endphp

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>inicio</title>
+</head>
+<body>
+    <h1>este es mi primer repositorio</h1>
+    <p>hola que hace</p>
+    <h1></h1>
+    @php
+        echo "esto es un texto"
+    @endphp
+</body>
+</html>
